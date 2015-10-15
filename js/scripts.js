21a6bb9c9da2b41b774d4d63ddb591d6e0322427
Youssef <student@epicodus.com>
2015-10-14 19:52:23 -0700

    Fix js functionality, now lists favorite icecream properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
   var icecreamFlavors = ["Vanilla", "chocolate", "mint", "cookie dough"]
-  var ids = [1, 2, 3, 4]
 
   icecreamFlavors.forEach(function(flavor) {
-    $("#icecream" + ids).text(flavor);
+    $('div#flavorlist ul').append("<li>" + flavor + "</li>");
 
   });
 });
